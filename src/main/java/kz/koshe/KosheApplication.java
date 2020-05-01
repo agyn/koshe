0432ea0a14987ea044c858922cbe8571c2db8ad3
agyn <agyn_1992@mail.ru>
2020-05-01 23:32:53 +0600

    koshe сундет

---
+++ src/main/java/kz/koshe/KosheApplication.java
@@ -0,0 +1,13 @@
+package kz.koshe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KosheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KosheApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kz/koshe/KosheApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.koshe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KosheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
